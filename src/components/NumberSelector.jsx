973452bbf3e116e79cc5ab55b3d49289766581b9
Akhilesh Singh Maurya <akhileshbltr2002@gmail.com>
2024-04-24 21:33:50 +0530

    First commit

--- src/components/NumberSelector.jsx
+++ src/components/NumberSelector.jsx
@@ -17,14 +17,6 @@ const NumberSelector = ({error,setError,selectedNumber , setSelectedNumber}) => 
      
      {arrNumber.map((value,i)=>(
       <div
-       onClick={ ()=>numberSelectorHandler(value) } className={`  w-[62px] h-[62px] border-solid border-2 border-black flex justify-center items-center  font-bold  ${selectedNumber ===value ? "bg-black" : "bg-white"} ${selectedNumber ===value ? "text-white" : " text-black"} `} key={i}>
-        {value}
-        </div>
-        
-     ))}
-    </div>
-    <p className=" flex flex-col text-[24px] font-[700] items-end ">Select Number</p>
-    </div>
   );
 };
 
